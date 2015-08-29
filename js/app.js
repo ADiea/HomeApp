17517d7d1c4eb3cf26653a5405b9a7b300e54161
unknown <diea_andrei@yahoo.com>
2015-08-30 00:47:33 +0300

    menu

--- js/app.js
+++ js/app.js
@@ -1,103 +1,73 @@
-//Phonegap app
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        console.log('Received Event: ' + id);
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('ionicApp', ['ionic', 'ionicApp.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
     }
-};
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
 
-angular.module('ionicApp', ['ionic'])
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
 
-.controller('MyCtrl', function($scope, $timeout) {
-    $scope.myTitle = 'Template';
-    
-    $scope.data = { 'volume' : '5' };
-    
-    var timeoutId = null;
-    
-    $scope.$watch('data.volume', function() {
-        
-        
-        console.log('Has changed');
-        
-        if(timeoutId !== null) {
-            console.log('Ignoring this movement');
-            return;
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'views/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.tests', {
+      url: '/tests',
+      views: {
+        'menuContent': {
+          templateUrl: 'views/tests.html',
+		  controller: 'TestsCtrl'
         }
-        
-        console.log('Not going to ignore this one');
-        timeoutId = $timeout( function() {
-            
-            console.log('It changed recently!');
-            
-            $timeout.cancel(timeoutId);
-            timeoutId = null;
-            
-            // Now load data from server 
-        }, 1000); 
-        
-        
-    });
-	
-	/*
-phonegap
-*/
-
-// Fetch Device info from Device Plugin
-	$scope.alertDeviceInfo = function() {
-		var deviceInfo = ('Device Platform: ' + device.platform + '\n'
-				+ 'Device Version: ' + device.version + '\n' + 'Device Model: '
-				+ device.model + '\n' + 'Device UUID: ' + device.uuid + '\n');
-
-		navigator.notification.alert(deviceInfo);
-	};
-
-	// Fetch location info from GeoLocation Plugin
-	$scope.alertGeoLocation = function() {
-		var onSuccess = function(position) {
-			navigator.notification.alert('Latitude: '
-					+ position.coords.latitude + '\n' + 'Longitude: '
-					+ position.coords.longitude + '\n' + 'Altitude: '
-					+ position.coords.altitude + '\n' + 'Accuracy: '
-					+ position.coords.accuracy + '\n' + 'Altitude Accuracy: '
-					+ position.coords.altitudeAccuracy + '\n' + 'Heading: '
-					+ position.coords.heading + '\n' + 'Timestamp: '
-					+ position.timestamp + '\n');
-		};
-		navigator.geolocation.getCurrentPosition(onSuccess);
-
-	};
-
-	// Makes a beep sound
-	$scope.beepNotify = function() {
-		navigator.notification.beep(1);
-	};
-
-	// Vibrates the phone
-	$scope.vibrateNotify = function() {
-		navigator.notification.vibrate(1000);
-	};
-	
-/*
-# # # # # # # # # # # # # #
-*/	
-    
+      }
+    })
+  .state('app.settings', {
+      url: '/settings',
+      views: {
+        'menuContent': {
+          templateUrl: 'views/settings.html',
+          controller: 'SettingsCtrl'
+        }
+      }
+    })
+   .state('app.light', {
+      url: '/light',
+      views: {
+        'menuContent': {
+          templateUrl: 'views/light.html',
+          controller: 'LightCtrl'
+        }
+      }
+    })
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'views/playlist.html',
+        controller: 'BlankCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/light');
 });
---
+++ js/controllers.js
@@ -0,0 +1,114 @@
+angular.module('ionicApp.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('views/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+  
+
+})
+
+.controller('SettingsCtrl', function($scope) {
+
+})
+.controller('BlankCtrl', function($scope) {
+
+})
+
+.controller('LightCtrl', function($scope, $stateParams) {
+
+$scope.data = {dimMax : 0};
+
+
+})
+.controller('TestsCtrl', function($scope, $stateParams) {
+
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+
+	$scope.moreInfo = "n/a";
+// Fetch Device info from Device Plugin
+	$scope.alertDeviceInfo = function() {
+		$scope.moreInfo = ('Device Platform: ' + device.platform + '\n'
+				+ 'Device Version: ' + device.version + '\n' + 'Device Model: '
+				+ device.model + '\n' + 'Device UUID: ' + device.uuid + '\n');
+
+		//navigator.notification.alert(deviceInfo);
+	};
+
+	// Fetch location info from GeoLocation Plugin
+	$scope.alertGeoLocation = function() {
+		var onSuccess = function(position) {
+			$scope.moreInfo = 'Latitude: '
+					+ position.coords.latitude + '\n' + 'Longitude: '
+					+ position.coords.longitude + '\n' + 'Altitude: '
+					+ position.coords.altitude + '\n' + 'Accuracy: '
+					+ position.coords.accuracy + '\n' + 'Altitude Accuracy: '
+					+ position.coords.altitudeAccuracy + '\n' + 'Heading: '
+					+ position.coords.heading + '\n' + 'Timestamp: '
+					+ position.timestamp + '\n';
+		};
+		var onFailure = function(err, msg) {
+			$scope.moreInfo = 'Error: '
+					+ err + '\n' + 'Err code: '
+					+ err.code + '\n' + 'Msg: '
+					+ msg + '\n' + 'Accuracy: '
+					+ position.coords.accuracy + '\n';
+		};
+		$scope.moreInfo = "Location: (loading)";
+		navigator.geolocation.getCurrentPosition(onSuccess, onFailure, { timeout: 15000, enableHighAccuracy: false });
+
+	};
+
+	// Makes a beep sound
+	$scope.beepNotify = function() {
+		navigator.notification.beep(1);
+	};
+
+	// Vibrates the phone
+	$scope.vibrateNotify = function() {
+		navigator.notification.vibrate(1000);
+	};
+
+});

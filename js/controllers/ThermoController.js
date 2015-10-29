be98c530c981d5836a9caf035748c62ef399f1db
unknown <diea_andrei@yahoo.com>
2015-10-29 20:20:43 +0200

    cosmetics

--- js/controllers/ThermoController.js
+++ js/controllers/ThermoController.js
@@ -483,21 +483,21 @@ var _ThermoCtrl = ionicApp.controller('ThermoCtrl', function($scope, settings, s
 		var seconds = Math.floor(((new Date().getTime()/1000) - date)),
 		interval = Math.floor(seconds / 31536000);
 
-		if (interval > 1) return interval + "y";
+		if (interval > 1) return interval + "y ago";
 
 		interval = Math.floor(seconds / 2592000);
-		if (interval > 1) return interval + "m";
+		if (interval > 1) return interval + "m ago";
 
 		interval = Math.floor(seconds / 86400);
-		if (interval >= 1) return interval + "d";
+		if (interval >= 1) return interval + "d ago";
 
 		interval = Math.floor(seconds / 3600);
-		if (interval >= 1) return interval + "h";
+		if (interval >= 1) return interval + "h ago";
 
 		interval = Math.floor(seconds / 60);
-		if (interval > 1) return interval + "m ";
+		if (interval > 1) return interval + "m ago";
 
-		return Math.floor(seconds) + "s";
+		return Math.floor(seconds) < 3 ? "now" : Math.floor(seconds) + "s ago";
 	}
 	
 	$scope.addLog = function addLog(msg)
